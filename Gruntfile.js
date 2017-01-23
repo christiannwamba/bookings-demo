a413cd6b9b1af586ece67baeac6ad851b49edeeb
Christian Nwamba <nwambachristian@gmail.com>
2017-01-23 09:13:59 +0100

    Initial

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,7 +257,7 @@ module.exports = function(grunt) {
    */
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-clean');
